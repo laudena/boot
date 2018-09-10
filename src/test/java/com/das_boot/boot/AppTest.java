b0507fde5e236c794a9fb463770ed7fa720f9dde
laudena <laudena@gmail.com>
2018-09-10 12:03:27 +0300

    testing 101

--- src/test/java/com/das_boot/boot/AppTest.java
+++ src/test/java/com/das_boot/boot/AppTest.java
@@ -1,38 +1,20 @@
 package com.das_boot.boot;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
+import org.junit.Test;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boot.controller.HomeController;
+
+@RestController
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+	@Test
     public void testApp()
     {
-        assertTrue( true );
+        HomeController hc= new HomeController();
+        String result = hc.home();
+        assertEquals(result,  "Das boot, reporting for duty");
     }
 }
